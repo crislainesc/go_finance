6524f4d251a64d14e6e35a4d93f74f91557ebabc
Crislâine Santos <criis.santos@outlook.com>
2023-08-07 15:07:56 -0300

    feat: add accounts, categories and user database queries

--- db/migration/000001_initial_tables.up.sql
+++ db/migration/000001_initial_tables.up.sql
@@ -1,35 +1,36 @@
 CREATE TABLE "users" (
   "id" serial PRIMARY KEY NOT NULL,
-  "userName" varchar(255) NOT NULL,
+  "username" varchar(255) NOT NULL,
   "password" varchar(255) NOT NULL,
   "email" varchar(255) NOT NULL UNIQUE,
-  "createdAt" timestamptz NOT NULL DEFAULT (now()),
-  "updatedAt" timestamptz NOT NULL DEFAULT (now())
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "categories" (
   "id" serial PRIMARY KEY NOT NULL,
-  "userId" int NOT NULL,
+  "user_id" int NOT NULL,
   "title" varchar(255) NOT NULL,
   "type" varchar(50) NOT NULL,
   "description" varchar(255) NOT NULL,
-  "createdAt" timestamptz NOT NULL DEFAULT (now()),
-  "updatedAt" timestamptz NOT NULL DEFAULT (now())
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE "categories" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
+ALTER TABLE "categories" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
 CREATE TABLE "accounts" (
   "id" serial PRIMARY KEY NOT NULL,
-  "userId" int NOT NULL,
-  "categoryId" int NOT NULL,
+  "user_id" int NOT NULL,
+  "category_id" int NOT NULL,
   "title" varchar(255) NOT NULL,
+  "type" varchar NOT NULL,
   "description" varchar(255) NOT NULL,
   "value" INTEGER NOT NULL,
   "date" DATE NOT NULL,
-  "createdAt" timestamptz NOT NULL DEFAULT (now()),
-  "updatedAt" timestamptz NOT NULL DEFAULT (now())
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE "accounts" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
-ALTER TABLE "accounts" ADD FOREIGN KEY ("categoryId") REFERENCES "categories" ("id");
+ALTER TABLE "accounts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "accounts" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
---
+++ db/query/category.sql
@@ -0,0 +1,34 @@
+-- name: CreateCategory :one
+INSERT INTO categories (
+  user_id,
+  title,
+  type,
+  description
+) VALUES (
+  $1, $2, $3, $4
+) RETURNING *;
+
+-- name: GetCategoryById :one
+SELECT * FROM categories
+WHERE id = $1 LIMIT 1;
+
+-- name: GetCategories :many
+SELECT * FROM categories
+WHERE
+  user_id = $1
+AND
+  type = $2
+AND
+  LOWER(title) LIKE CONCAT('%', LOWER(@title::text), '%')
+AND
+  LOWER(description) LIKE CONCAT('%', LOWER(@type::text), '%');
+
+-- name: UpdateCategory :one
+UPDATE categories
+SET title = $2, description = $3, "updated_at" = (NOW())
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteCategory :exec
+DELETE FROM categories
+WHERE id = $1;
